c9fffb4a8a967133ab8e69ed57a64b189a7c0b79
Frederik Rietdijk <fridh@fridh.nl>
2018-04-04 20:06:04 +0200

    python: python-dateutil: 2.6.1 -> 2.7.2

--- pkgs/development/python-modules/dateutil/default.nix
+++ pkgs/development/python-modules/dateutil/default.nix
@@ -1,15 +1,24 @@
-{ stdenv, buildPythonPackage, fetchPypi, six }:
+{ stdenv, buildPythonPackage, fetchPypi, six, setuptools_scm, pytest, freezegun }:
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "python-dateutil";
-  version = "2.6.1";
+  version = "2.7.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1jkahssf0ir5ssxc3ydbp8cpv77limn8d4s77szb2nrgl2r3h749";
+    sha256 = "9d8074be4c993fbe4947878ce593052f71dac82932a677d49194d8ce9778002e";
   };
 
+  buildInputs = [ setuptools_scm ];
   propagatedBuildInputs = [ six ];
+  checkInputs = [ pytest freezegun ];
+
+  checkPhase = ''
+    py.test
+  '';
+
+  # Infinite recursion with freezegun
+  doCheck = false;
 
   meta = with stdenv.lib; {
     description = "Powerful extensions to the standard datetime module";
